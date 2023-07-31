af6a4f69874d677e8f89f902110447a74007f310
Reinaldo Souza <reinaldo_7531@hotmail.com>
2023-07-31 15:17:10 +0100

    Crud Players

---
+++ Liga.web/Controllers/PlayersController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Liga.web.Data;
+using Liga.web.Models.Entity;
+
+namespace Liga.web.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public PlayersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Players
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Players.ToListAsync());
+        }
+
+        // GET: Players/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var playerEntity = await _context.Players
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (playerEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(playerEntity);
+        }
+
+        // GET: Players/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Players/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] PlayerEntity playerEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(playerEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(playerEntity);
+        }
+
+        // GET: Players/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var playerEntity = await _context.Players.FindAsync(id);
+            if (playerEntity == null)
+            {
+                return NotFound();
+            }
+            return View(playerEntity);
+        }
+
+        // POST: Players/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] PlayerEntity playerEntity)
+        {
+            if (id != playerEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(playerEntity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlayerEntityExists(playerEntity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(playerEntity);
+        }
+
+        // GET: Players/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var playerEntity = await _context.Players
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (playerEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(playerEntity);
+        }
+
+        // POST: Players/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var playerEntity = await _context.Players.FindAsync(id);
+            _context.Players.Remove(playerEntity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlayerEntityExists(int id)
+        {
+            return _context.Players.Any(e => e.Id == id);
+        }
+    }
+}
--- Liga.web/Data/Entity/PlayerEntity.cs
+++ Liga.web/Data/Entity/PlayerEntity.cs
@@ -1,9 +1,13 @@
-﻿namespace Liga.web.Models.Entity
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Liga.web.Models.Entity
 {
     public class PlayerEntity : IPlayerEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [Display(Name ="Team")]
         public TeamEntity PlayerTeam { get; set; }
 
     }
